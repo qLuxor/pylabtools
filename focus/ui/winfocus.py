cb41a0258111e1fca991678ff6aef07876e89cb1
Matteo Schiavon @Sagnac <matteoschiav@yahoo.it>
2016-03-21 18:12:19 +0100

    Add measurement function

--- focus/ui/winfocus.py
+++ focus/ui/winfocus.py
@@ -6,12 +6,16 @@ Module implementing WinFocus.
 
 import sys
 sys.path.append('/home/sagnac/Quantum/')
+sys.path.append('/home/sagnac/Quantum/pyAPT/')
 
 import time
 
 from pyThorPM100.pm100 import pm100d
+import pyAPT
 
 import numpy as np
+from scipy.special import erfc
+from scipy.optimize import curve_fit
 
 from PyQt4.QtCore import pyqtSlot
 from PyQt4.QtGui import QMainWindow, qApp
@@ -36,6 +40,7 @@ class WinFocus(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         
         self.oscilloscope = False
+        self.started = False
         
         self.figOscilloscope = plt.figure()
         self.plotOscilloscope = FigureCanvas(self.figOscilloscope)
@@ -61,8 +66,80 @@ class WinFocus(QMainWindow, Ui_MainWindow):
         """
         
         # create the object for the power meter
-        #pwm = pm100d()
-        pass
+        if not self.started:
+            self.started = True
+            
+            self.btnStart.setStyleSheet("background-color: green")
+            
+            average = int(self.txtAverage.text())
+            minStage = float(self.txtMin.text())
+            maxStage = float(self.txtMax.text())
+            step = float(self.txtStep.text())
+            
+            x = np.arange(minStage, maxStage, step)
+            count = np.zeros(x.size)
+            
+            # open power meter
+            pwm = pm100d()
+            
+            selLinear = str(self.cmbLinearStage.currentText())
+            if selLinear == 'thorlabs':
+                # open APT controller
+                SN = int(self.txtSN.text())
+                con = pyAPT.MTS50(serial_number=SN)
+                con.goto(x[0], wait=True)
+
+            for i in range(x.size):
+                qApp.processEvents()
+                con.goto(x[i],  wait=True)
+                # wait until the movement has finished
+                stat = con.status()
+                while stat.moving:
+                    time.sleep(0.01)
+                    stat = con.status
+                singleMeasure = np.zeros(average)
+                for j in range(average):
+                    p = max(pwm.read()*1000, 0.)
+                    singleMeasure[j] = p
+                count[i] = np.mean(singleMeasure)
+                self.axErf.plot(x, count, '.')
+                self.plotErf.draw()
+                
+                if not self.started:
+                    break
+            
+            if self.started:
+                # find medium point
+                x_thres = x[np.nonzero(count>(np.max(count)+np.min(count))/2)[0]]
+                x0 = np.min(x_thres)
+                
+                # calculate fit
+                func = lambda x, a, b, c, d: a*erfc(b*(x-c)) + d
+                p0 = [np.max(count)/2,  1,  x0,  np.min(count)/2]
+                p, perr = curve_fit(func, x, count, p0=p0)
+                
+                self.axErf.hold(True)
+                self.axErf.plot(x, func(x, *p), 'r')
+                self.plotErf.draw()
+                self.axErf.hold(False)
+                
+                self.lblA0.setText("{:.4}".format(p[0]))
+                self.lblA1.setText("{:.4}".format(p[1]))
+                self.lblA2.setText("{:.4}".format(p[2]))
+                self.lblA3.setText("{:.4}".format(p[3]))
+                
+                waist = np.sqrt(2)/p[1]
+                
+                self.lblWaist.setText("{:.4}".format(waist))
+                
+            self.btnStart.setStyleSheet("")
+            self.started = False
+            
+        else:
+            self.btnStart.setStyleSheet("")
+            self.started = False
+        
+      
         
     
     @pyqtSlot()
@@ -72,9 +149,6 @@ class WinFocus(QMainWindow, Ui_MainWindow):
         """
         
         # create the object for the power meter
-        # pwm = pm100d()
-        
-        
         if not self.oscilloscope:
             self.oscilloscope = True
             
@@ -102,3 +176,4 @@ class WinFocus(QMainWindow, Ui_MainWindow):
         else:
             self.btnOscilloscope.setStyleSheet("")
             self.oscilloscope = False
+
