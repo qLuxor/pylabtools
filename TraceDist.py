2469351d6fb2b72d75c0990b83953421f16bdcc2
Giulio Foletto <giulio.foletto@outlook.com>
2017-05-07 22:41:40 +0200

    Added script for trace distance

---
+++ TraceDist.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  7 21:18:57 2017
+
+@author: Giulio Foletto
+"""
+
+import qutip
+import sys
+
+if len(sys.argv) ==5:
+    inputfilenameA = str(sys.argv[1])
+    indexA = int(sys.argv[2])
+    inputfilenameB = str(sys.argv[3])
+    indexB = int(sys.argv[4])
+    
+    listA=qutip.qload(inputfilenameA)
+    A=listA[indexA]
+    listB=qutip.qload(inputfilenameB)
+    B=listB[indexB]
+elif len(sys.argv) ==4:
+    inputfilename = str(sys.argv[1])
+    indexA = int(sys.argv[2])
+    indexB = int(sys.argv[3])
+    
+    mylist=qutip.qload(inputfilename)
+    A=mylist[indexA]
+    B=mylist[indexB]
+
+print("Loaded two objects")
+print(A)
+print(B)
+
+result = qutip.tracedist(A, B)
+print("The trace distance between them is")
+print(result)
