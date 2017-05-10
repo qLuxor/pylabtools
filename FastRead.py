5da784201e8db616ab66421584b72ca4a54ff0af
Giulio Foletto <giulio.foletto@outlook.com>
2017-05-10 12:31:45 +0200

    Added script for fast reading of PWM or SPAD in terminal

---
+++ FastRead.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 10 12:09:48 2017
+
+@author: Giulio Foletto
+"""
+
+import sys
+import json
+import time
+import numpy as np
+
+sys.path.append('/home/sagnac/Quantum/ttag/python/')
+import ttag
+
+sys.path.append('..')
+from pyThorPM100.pm100 import pm100d
+
+if len(sys.argv) >1:
+    filename = str(sys.argv[1])
+else:
+    filename ='settings.json'
+if filename[-5:]!=".json":
+    filename +=".json"
+
+with open(filename) as json_settings:
+    settings = json.load(json_settings)
+    json_settings.close()
+    
+sensor = settings["sensor"]
+allowtime = settings["allowtime"]
+
+#pwm configuration and initialization
+pwmAverage=settings["pwmAverage"]
+pwmWait=settings["pwmWait"]
+if sensor == "pwm" or sensor == "PWM":
+    print("Initializing PWM")
+    pwm = pm100d()
+
+#SPAD configuration and initialization
+spadBufNum =settings["spadBufNum"]
+spadDelay=settings["spadDelay"]
+spadDelay = spadDelay*1e-9
+spadChannelA=settings["spadChannelA"]
+spadChannelB=settings["spadChannelB"]
+delayarray = np.array([spadDelay, 0.0, 0.0,0.0])
+spadExpTime = settings["spadExpTime"]
+spadExpTime = spadExpTime*1e-3
+coincWindow = 2*1e-9
+if sensor == "spad" or sensor == "SPAD":
+    print("Initializing SPAD")
+    ttagBuf = ttag.TTBuffer(spadBufNum) 
+    
+print("Starting Measurements")
+while(1):
+    if sensor == "spad" or sensor == "SPAD":
+        time.sleep(spadExpTime)
+        coinc= ttagBuf.coincidences(spadExpTime,coincWindow,-delayarray)
+        print("Counts on Channel ", spadChannelA, " = ", coinc[spadChannelA, spadChannelA], 
+        "\tCounts on Channel ", spadChannelB, " = ", coinc[spadChannelB, spadChannelB], 
+        "\tCoincidences = ", coinc[spadChannelA, spadChannelB])
+    elif sensor == "pwm" or sensor == "PWM":
+        singleMeasure = np.zeros(pwmAverage)
+        for j in range(pwmAverage):
+            time.sleep(pwmWait)
+            p = max(pwm.read()*1000, 0.)
+            singleMeasure[j] = p
+        print("PWM measured = ", np.mean(singleMeasure), " mW")
