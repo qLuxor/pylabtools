a747bb07fa5cf20e19a394916766313b76a04dd4
Matteo Schiavon @Sagnac <matteoschiav@yahoo.it>
2017-07-06 19:03:21 +0200

    Added script to log temperature

---
+++ lm35.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  6 16:44:36 2017
+
+@author: sagnac
+"""
+
+import serial
+import datetime
+import time
+import numpy as np
+
+port='/dev/ttyACM0'
+baud=9600
+
+#initialize serial
+serialobj=serial.Serial()
+serialobj.port=port
+serialobj.baudrate=baud
+serialobj.timeout=1
+serialobj.setDTR(False)
+
+#launch serial
+serialobj.open()
+time.sleep(2)
+serialobj.flush()
+
+list_T = []
+list_time = []
+while(True):
+    list_time.append(datetime.datetime.now())
+    tempC=serialobj.readline()
+    tempC=float(tempC[0:5])
+    list_T.append(tempC)
+    np.savez('Temperatures.npz', temp=list_T, time=list_time)
+    print(tempC)
+    time.sleep(1)
